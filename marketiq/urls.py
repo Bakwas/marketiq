fde325f1a748bd64005ea2069bf476b61711ce8f
Nabeel Mukhtar <namukhtar@etilizepak.com>
2015-02-12 12:20:48 +0500

    Basic Django Appp for spell correction.

--- marketiq/urls.py
+++ marketiq/urls.py
@@ -1,12 +1,13 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
-from spellcorrection.views import SuggestionList
+from spellcorrection.views import HomeView, SuggestView
 
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'marketiq.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
+                       # Examples:
+                       # url(r'^$', 'marketiq.views.home', name='home'),
+                       # url(r'^blog/', include('blog.urls')),
 
-    url(r'^admin/', include(admin.site.urls)),
-    url(r'^suggestions/$', SuggestionList.as_view()),
-)
+                       url(r'^$', HomeView.as_view()),
+                       url(r'^admin/', include(admin.site.urls)),
+                       url(r'^suggest/$', SuggestView.as_view()),
+                       )
--- spellcorrection/views.py
+++ spellcorrection/views.py
@@ -1,18 +1,39 @@
-from django.shortcuts import render
-
 # Create your views here.
-from django.http import Http404
+from django.http import HttpResponseBadRequest
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from rest_framework import status
 import enchant
-import json
 
-class SuggestionList(APIView):
+
+class SuggestView(APIView):
+
+    def __init__(self):
+        self.dictionary = enchant.Dict('en_US')
+
     """
-    List all suggestions.
+    List all suggestions for the word supplied as parameter.
     """
+
     def get(self, request, format=None):
-        d = enchant.Dict("en_US")
-        return Response(json.dumps(d.suggest("Helo")))
+        word = request.GET.get('q', None)
+        if word:
+            response = {}
+            response['misspelt'] = not self.dictionary.check(word)
+            response['suggestions'] = self.dictionary.suggest(word)
+            return Response(response)
+        else:
+            return HttpResponseBadRequest(
+                "Invalid query for spell correction !!")
 
+
+class HomeView(APIView):
+
+    def __init__(self):
+        self.endpoints = {'suggest': '/suggest?q={query}'}
+
+    """
+    List all available URLs.
+    """
+
+    def get(self, request, format=None):
+        return Response({'endpoints': self.endpoints})
